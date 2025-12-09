d74d0e98478c6cf04f73871f89cc935e14129478
Jeroen Krajenbrink <jeroenkrajenbrink@gmail.com>
2025-12-09 12:33:57 +0100

    Fix token parameter name in API request

--- src/utils/sendMessage.js
+++ src/utils/sendMessage.js
@@ -4,7 +4,7 @@ import { API_TOKEN, API_URL } from "./constants";
 export async function sendMessage(message, recipient) {
   try {
     const response = await axios.get(API_URL, {
-      params: { recipient, message, API_TOKEN },
+      params: { recipient, message, token: API_TOKEN },
     });
 
     console.log(`Bericht verzonden naar ${recipient}:`, response.data);
